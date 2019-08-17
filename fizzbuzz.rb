6d32852b297111f15b1e74497e763caf92e559f1
Rizki Pratama <rizkipratama0602@gmail.com>
2019-08-17 19:39:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
+    returns "FizzBuzz"
   elsif number % 3 == 0
-    return "Fizz"
+    returns "Fizz"
   elsif number % 5 == 0
-    return "Buzz"
+    returns "Buzz"
   else
-    return nil
+    returns nil
   end
 end
